226b7a888124a63493a5ee1962481d37f82848bc
Hadi <47762677+Hadi471@users.noreply.github.com>
2024-11-04 15:27:38 -0500

    Add files via upload

---
+++ Lab 1/Task 1/Lab1T1.c
@@ -0,0 +1,82 @@
+
+#include <xc.h>
+#include <P24FJ256GA110.h>
+
+void msDelay (int msec){
+    int i,j;
+    for (i=0 ; i<msec ; i++){
+        for (j=0 ; j<50 ; j++){
+            
+        }
+    }
+}
+
+int main(void) {
+
+    TRISD = 0x0000;
+    PORTD = 0x0000;
+    while(1){
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00010000000;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01000000000;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01010000000;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00000000010;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00010000010;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01000000010;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01010000010;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00000000100;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00010000100;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01000000100;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01010000100;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00000000110;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b00010000110;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01000000110;
+        msDelay(1000);
+        PORTD = 0;
+        msDelay(250);
+        PORTD = 0b01010000110;
+        msDelay(1000);
+        PORTD = 0;
+    }
+return 0;
+}
